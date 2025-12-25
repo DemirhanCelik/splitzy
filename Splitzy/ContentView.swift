1018be420018823900eeb271c3e88226de9a251f
Demirhan <demirhanceliik@gmail.com>
2025-12-25 14:56:11 +0300

    Initial Commit

---
+++ Splitzy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Splitzy
+//
+//  Created by Demirhan Celik on 12/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Splitzy/SplitzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitzyApp.swift
+//  Splitzy
+//
+//  Created by Demirhan Celik on 12/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
